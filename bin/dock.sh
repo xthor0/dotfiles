e3fee12802b0a9eca759e0e5c8464eaab98186af
Ben Brown <bbrown@nav.com>
2020-01-08 17:40:01 -0700

    wip

--- bin/dock.sh
+++ bin/dock.sh
@@ -2,7 +2,7 @@
 
 # this probably needs adjusting. It's going to be different at home vs at work.
 # one thought I had was to make it look at what wifi network I'm connected to, in order to make the determination.
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1 --mode 3840x2160 --pos 5760x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output DP-2-1 --mode 3840x2160 --pos 1920x0 --rotate normal --output DP-2-2 --off --output DP-2-3 --off
+xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1 --mode 3840x2160 --pos 5760x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output DP-2-1 --mode 3840x2160 --pos 1920x0 --rotate normal --primary --output DP-2-2 --off --output DP-2-3 --off
 
 killall -SIGUSR1 conky
 

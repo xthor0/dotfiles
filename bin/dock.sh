6aabf3e0960380192449525124b8eae495e0cb15
Ben Brown <bbrown@nav.com>
2020-01-09 09:20:31 -0700

    wip

--- bin/dock.sh
+++ bin/dock.sh
@@ -10,7 +10,7 @@
 # so, if I collect those values at home, and then at work, I should know where I'm docked.
 
 HOME_IDS="a2727e78d7d4852906365f43860bc9ac 8d08f336fa443732b9381d216bc05345"
-WORK_IDS=""
+WORK_IDS="048c0878daca09123ab2627ad8fe5800 80f4ed58aea82d6ea488f91be9207144"
 
 # let's figure out which monitors are attached and what port they are attached to
 DEVICES=$(find /sys/class/drm/*/status)
@@ -43,7 +43,7 @@ if [ "${location}" == "HOME" ]; then
 fi
 
 if [ "${location}" == "WORK" ]; then
-    xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1 --mode 3840x2160 --pos 5760x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output DP-2-1 --mode 3840x2160 --pos 1920x0 --rotate normal --primary --output DP-2-2 --off --output DP-2-3 --off
+    xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --mode 3840x2160 --pos 5760x0 --rotate normal --output DP-1-1 --primary --mode 3840x2160 --pos 1920x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --off
 
     nitrogen --head=0 --set-centered ~/Pictures/hexagram11080p.jpg 
     nitrogen --head=1 --set-centered ~/Pictures/hexagram1uhd.jpg 
